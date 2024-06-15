1bb25fb6efbfeea10e94a9a7bffd3d9fb9099377
Heine Krogstad <40359669+HeineKrogstad@users.noreply.github.com>
2024-06-15 19:01:13 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,23 +1,32 @@
-const jsonServer = require('json-server');
-const auth = require('json-server-auth');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const rules = auth.rewriter({
-  // разрешения на чтение/запись
-  users: 600,
-  posts: 640,
-  comments: 660
-});
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares);
-// Set up authorization rules
-server.use(rules);
+const server = jsonServer.create()
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
 // Add this before server.use(router)
-server.use(auth);
-server.use(router);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server with Auth is running');
-});
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
 
